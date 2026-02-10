74066300d2f2225e4ba5f90be1a83c8e58282efa
firas1996 <firas.hm.1996@gmail.com>
2026-02-10 10:48:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("Connected to the DB !!!!");
+  })
+  .catch((e) => {
+    console.log("Error: " + e);
+  });
+
+const app = express();
+
+const port = 1122;
+
+app.listen(port, () => {
+  console.log("The server is running on Port: " + port);
+});
