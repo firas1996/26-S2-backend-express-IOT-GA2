3a61061f0febdc0824c148504c0986df162d5a2c
firas1996 <firas.hm.1996@gmail.com>
2026-03-03 08:36:45 +0100

    a

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,15 @@
-const { createUser, getAllUsers } = require("../controllers/userController");
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUserById,
+} = require("../controllers/userController");
 
 const router = require("express").Router();
 
 router.post("/createUser", createUser);
 router.get("/getAllUsers", getAllUsers);
+router.get("/getUserById/:id", getUserById);
+router.patch("/updateUserById/:id", updateUserById);
 
 module.exports = router;
