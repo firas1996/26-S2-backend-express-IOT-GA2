94e9b7658519627db235e96d0b961a1c34abc575
firas1996 <firas.hm.1996@gmail.com>
2026-04-21 11:36:10 +0100

    aa

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
 dotenv.config({ path: "./.env" });
 
@@ -15,6 +16,7 @@ mongoose
   });
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRoutes);
 
